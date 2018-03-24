bcdddffc8ab770b8a67d377678336858712cef6a
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-24 15:55:48 -0300

    first test green

--- src/microservices/ddd-calculator/index.js
+++ src/microservices/ddd-calculator/index.js
@@ -1,9 +1,14 @@
 import { Responder } from 'cote';
 const responder = new Responder({ name: 'ddd-calculator' });
-const rates = { usd_eur: 0.91, eur_usd: 1.10 };
 
 responder.on('calculate', (req, cb) => {
     cb(req.amount * rates[`${req.from}_${req.to}`]);
 });
 
-responder.on('say-hi', () => 'hi, from the ddd-calculator');
+responder.on('say-hi', (req, cb) => cb('hi, from the ddd-calculator'));
+
+const rates = { usd_eur: 0.91, eur_usd: 1.10 };
+
+responder.on('convert', (req, cb) => {
+    cb(req.amount * rates[`${req.from}_${req.to}`]);
+});
