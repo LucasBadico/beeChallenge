37c4277d2409f9293a454a8e3b537d9a4edbd023
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-24 16:07:19 -0300

    first test refactored and still green

--- src/microservices/ddd-calculator/index.js
+++ src/microservices/ddd-calculator/index.js
@@ -5,7 +5,7 @@ responder.on('calculate', (req, cb) => {
     cb(req.amount * rates[`${req.from}_${req.to}`]);
 });
 
-responder.on('say-hi', (req, cb) => cb('hi, from the ddd-calculator'));
+responder.on('say-hi', _ => Promise.resolve('hi, from the ddd-calculator'));
 
 const rates = { usd_eur: 0.91, eur_usd: 1.10 };
 
