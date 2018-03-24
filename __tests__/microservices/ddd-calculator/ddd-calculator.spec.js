bfd10434001e0455e37a31eb0bbccd49d0575843
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-24 20:35:26 -0300

    koa boilerplate done, with test

---
+++ __tests__/microservices/ddd-calculator/ddd-calculator.spec.js
@@ -0,0 +1,17 @@
+import {
+    server,
+    app,
+} from '$/ddd-calculator'
+
+import httpRequester from 'supertest'
+
+const request = httpRequester.agent(server)
+
+describe('Calculator app', () => {
+    it('should have a default \'Hello World! from koa\'', async done => {
+        expect.assertions(1)
+        const helloFromApp = await request.get('/')     
+        expect(helloFromApp.text).toEqual('Hello World! from koa')
+        done()
+    })
+  })
