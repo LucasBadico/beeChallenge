ce0c3d2685ba955e595e74091e744ec06a9ee8a9
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-24 20:38:48 -0300

    removed unecessary util function

--- src/utils/promissifySend.js
+++
@@ -1,6 +0,0 @@
-export const promissifySend = requester => request => new Promise((resolve, reject) => {
-    requester.send(request, (res, err) => {
-        if (err) return reject(err)
-        return resolve(res)
-    });
-})
