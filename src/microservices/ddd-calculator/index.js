144f0425d46b322a58f4fb3b37fde34bf504079c
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-24 16:08:14 -0300

    boilerplate cote done

--- src/microservices/ddd-calculator/index.js
+++ src/microservices/ddd-calculator/index.js
@@ -1,14 +1,4 @@
 import { Responder } from 'cote';
 const responder = new Responder({ name: 'ddd-calculator' });
 
-responder.on('calculate', (req, cb) => {
-    cb(req.amount * rates[`${req.from}_${req.to}`]);
-});
-
 responder.on('say-hi', _ => Promise.resolve('hi, from the ddd-calculator'));
-
-const rates = { usd_eur: 0.91, eur_usd: 1.10 };
-
-responder.on('convert', (req, cb) => {
-    cb(req.amount * rates[`${req.from}_${req.to}`]);
-});
