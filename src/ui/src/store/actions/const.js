bdd3cd3a2bae6d98c3fe222fe1e32df212a88728
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 00:22:41 -0300

    more close to a react app running

---
+++ src/ui/src/store/actions/const.js
@@ -0,0 +1,29 @@
+// @flow
+
+// keyWords
+export const keyPadrao = 'na';
+export const holding = 'holding';
+export const company = 'company';
+export const pointOfSale = 'pointOfSale';
+export const startDate = 'startDate';
+export const date = 'date';
+
+// type scopo position
+export const ROOTITEM = 'ROOTITEM';
+export const BUNDLE = 'BUNDLE';
+export const ITEM = 'ITEM';
+export const SUBITEM = 'SUBITEM';
+export const COMPLEXSUBITEM = 'COMPLEXSUBITEM';
+export const TABSUBITEM = 'TABSUBITEM';
+
+// fields status
+export const INVALID = 'INVALID';
+export const VALID = 'VALID';
+export const DIRTY = 'DIRTY';
+export const TOUCHED = 'TOUCHED';
+export const PRISTINE = 'PRISTINE';
+export const FOCUS = 'FOCUS';
+
+// reports types
+export const dialog = 'dialog';
+export const snackbar = 'snackbar';
