9aba75c57984b36b09420a76e78f3d7652c834e1
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 16:39:32 -0300

    test of prices model

--- __tests__/services/prices/model.spec.js
+++ __tests__/services/prices/model.spec.js
@@ -1,7 +1,12 @@
+import prices from '$/model'
+import * as R from 'ramda'
+
 describe('Prices model', () => {
-    it('should have a default \'say-hi\'', async done => {
-        expect.assertions(1)
-        expect(true).toBe(true)
+    it('should \'prices model\' has keys \'table\' and \'schema\'', async done => {
+        expect.assertions(2)
+        const keys = R.keys(prices)
+        expect(keys).toContain('schema')
+        expect(keys).toContain('table')
         done()
     })
   })
