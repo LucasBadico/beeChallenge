d05173d60512b6bc16cdc53e2ea63cf661c16a88
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 15:37:12 -0300

    prices service add and base service reactored

---
+++ __tests__/services/prices/model.spec.js
@@ -0,0 +1,7 @@
+describe('Prices model', () => {
+    it('should have a default \'say-hi\'', async done => {
+        expect.assertions(1)
+        expect(true).toBe(true)
+        done()
+    })
+  })
