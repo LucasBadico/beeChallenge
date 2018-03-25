ee3caf9cbdade7a7230195baad07d10ab61a74ee
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 16:39:32 -0300

    test of prices model

--- __tests__/database/RethinkdbDataModel.spec.js
+++ __tests__/database/RethinkdbDataModel.spec.js
@@ -1,4 +1,14 @@
 
+// import RethinkdbDataModel
+
+// instanciate directly
+
+// test it
+
+// extend 
+
+// test it
+
 describe('RethinkdbDataModel class', () => {
     it('should have a default \'say-hi\'', async done => {
         expect.assertions(1)
