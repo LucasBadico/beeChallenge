38753c5e28bd0da072d36156486690b8745de786
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 01:56:48 -0300

    boilerplate react done!

--- src/ui/src/store/actions/const.js
+++ src/ui/src/store/actions/const.js
@@ -1,21 +1,3 @@
-// @flow
-
-// keyWords
-export const keyPadrao = 'na';
-export const holding = 'holding';
-export const company = 'company';
-export const pointOfSale = 'pointOfSale';
-export const startDate = 'startDate';
-export const date = 'date';
-
-// type scopo position
-export const ROOTITEM = 'ROOTITEM';
-export const BUNDLE = 'BUNDLE';
-export const ITEM = 'ITEM';
-export const SUBITEM = 'SUBITEM';
-export const COMPLEXSUBITEM = 'COMPLEXSUBITEM';
-export const TABSUBITEM = 'TABSUBITEM';
-
 // fields status
 export const INVALID = 'INVALID';
 export const VALID = 'VALID';
