f9908c69ecd5e4cff9431a6aaf4ecd9c973b8f26
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 16:26:40 -0300

    base model of rethinkdb data converted from prototype to class

---
+++ __tests__/database/RethinkdbDataModel.spec.js
@@ -0,0 +1,8 @@
+
+describe('RethinkdbDataModel class', () => {
+    it('should have a default \'say-hi\'', async done => {
+        expect.assertions(1)
+        expect(true).toBe(true)
+        done()
+    })
+  })
---
+++ src/database/RethinkdbDataModel.js
@@ -0,0 +1,50 @@
+import db from './'
+
+const r = db(),
+
+export class RethinkdbDataModel {
+    constructor({
+        table,
+        schema,
+    }){
+        this.table = table
+        this.schema = [...schema, 'created_at','updated_at']
+    }
+
+    getSchema() {
+        return this.schema
+    }
+
+    async getAll() {
+        const all = await r.table(this.table).run()
+        return all
+    }
+
+    async getById(id) {
+        const result =  await r.table(this.table)
+			.get(id)
+			.run();
+	    return result
+    }
+
+    async update(id, data) {
+        data.updated_at = r.now();
+        const result = await r.table(this.table)
+                .get(id)
+                .update(data)
+                .run();
+        
+        return result;
+    }
+
+    async save(data) {
+        const result = await r.table(this.table)
+                .insert({
+                    ...data,
+                    created_at: r.now(),
+                    updated_at: r.now(),
+                })
+                .run()
+        return result;
+    }
+}
