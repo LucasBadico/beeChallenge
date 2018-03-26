becf77647b43137e5a046f08a61a9c6c5eb63999
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-25 21:52:53 -0300

    price class tests all green

--- src/services/price/Price.js
+++ src/services/price/Price.js
@@ -3,21 +3,17 @@ import model from './model'
 
 class Price {
     constructor(props){
-        this = {
-            ...this,
-            ...props,
-        }
+        Object.assign(this, props)
     }
-
+    
     static async all(asRaw) {
         try {
             const result = await model.getAll()
             if (R.isEmpty(result)) return []
             if (asRaw) return result
-            return R.reduce(
-                (acc, item) => [...acc, new Price(item, true)]
+            return result.reduce(
+                (acc, item) => [...acc, new Price(item, true)],
                 [],
-                result,
             )
         } catch (err){
             throw new Error(err)
@@ -40,7 +36,10 @@ class Price {
             const result = await model.getByOrigin(origin)
             if (R.isEmpty(result) || R.isNil(result)) throw new Error('Origin not found', origin)
             if (asRaw) return result
-            return new Price(result, true)
+            return result.reduce(
+                (acc, item) => [...acc, new Price(item, true)],
+                [],
+            )
         } catch (err){
             throw new Error(err)
         }
@@ -51,7 +50,10 @@ class Price {
             const result = await model.getByDestination(destination)
             if (R.isEmpty(result) || R.isNil(result)) throw new Error('Destination not found', destination)
             if (asRaw) return result
-            return new Price(result, true)
+            return result.reduce(
+                (acc, item) => [...acc, new Price(item, true)],
+                [],
+            )
         } catch (err){
             throw new Error(err)
         }
