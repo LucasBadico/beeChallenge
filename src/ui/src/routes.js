c62bdbe84bd55d7ff82c5ce361d71ff85dc85631
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-27 22:10:53 -0300

    ui removed

--- src/ui/src/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
-// import './elemental.less'
-
-export default (
-  <Switch>
-    <Route path='/hey' component={() => <h1>HELLO FROM REACT!</h1>} />
-  </Switch>
-)
