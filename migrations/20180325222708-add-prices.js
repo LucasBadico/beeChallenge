695009c2b1aff5301126ed91e2e28c78e645268f
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-29 18:52:01 -0300

    Data insert on migrate up

--- migrations/20180325222708-add-prices.js
+++ migrations/20180325222708-add-prices.js
@@ -1,8 +1,41 @@
 const TABLE = 'prices';
+const DATA = [
+  {
+    "destination":  "011" ,
+    "origin":  "016" ,
+    "costByMinute": 2.9
+  },
+  {
+    "destination":  "011" ,
+    "origin":  "018" ,
+    "costByMinute": 1.9
+  },
+  {
+    "destination":  "016" ,
+    "origin":  "011" ,
+    "costByMinute": 1.9
+  },
+  {
+    "destination":  "017" ,
+    "origin":  "011" ,
+    "costByMinute": 1.7
+  },
+  {
+    "destination":  "011" ,
+    "origin":  "017" ,
+    "costByMinute": 2.7
+  },
+  {
+    "destination":  "018" ,
+    "origin":  "011" ,
+    "costByMinute": 0.9
+  }
+]
 
 exports.up = function (r, connection) {
     return r.tableCreate(TABLE)
       .run(connection)
+      .then(() => r.table(TABLE).insert(DATA).run(connection))
       .catch(console.error)
 }
 
@@ -14,35 +47,3 @@ exports.down = function (r, connection) {
 
 // add data
 
-/*
-  {
-  "destination":  "011" ,
-  "origin":  "016" ,
-  "costByMinute": 2.9
-  },
-  {
-  "destination":  "011" ,
-  "origin":  "018" ,
-  "costByMinute": 1.9
-  },
-  {
-  "destination":  "016" ,
-  "origin":  "011" ,
-  "costByMinute": 1.9
-  },
-  {
-  "destination":  "017" ,
-  "origin":  "011" ,
-  "costByMinute": 1.7
-  },
-  {
-  "destination":  "011" ,
-  "origin":  "017" ,
-  "costByMinute": 2.7
-  },
-  {
-  "destination":  "018" ,
-  "origin":  "011" ,
-  "costByMinute": 0.9
-  }
-*/
