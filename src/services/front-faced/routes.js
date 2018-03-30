673d3186eb78181b46b87898b86352433201ea8b
LucasBadico <lucasgomesdesantana@gmail.com>
2018-03-30 13:09:13 -0300

    routes changed to /api/ and removed folders dist and webpackbuild

--- src/services/front-faced/routes.js
+++ src/services/front-faced/routes.js
@@ -4,24 +4,22 @@ import { Requester } from 'cote'
 const requester = new Requester({ name: 'front-faced requester' })
 const router = new Router()
 
-router.get('/hello', (ctx, next) => {
-    ctx.body = 'Hello World! from koa';
+router.get('/api/hello', (ctx, next) => {
+    ctx.body = 'Hello World! from koa root/api/hello';
 })
 
 // prices
-router.get('/prices/all', async (ctx, next) => {
-    console.log('REQUEST INCOMING', ctx.request)    
-    console.log('REQUEST BODY', ctx.request.body)    
+router.get('/api/prices/all', async (ctx, next) => {
     const allPrices = await requester.send({ type: 'prices-all' }) 
     ctx.body = allPrices
 })
 
-router.post('/prices/by-origin', async (ctx, next) => {
+router.post('/api/prices/by-origin', async (ctx, next) => {
     const prices = await requester.send({ type: 'prices-by-origin', origin: ctx.request.body.origin })
     ctx.body = prices
 })
 
-router.post('/prices/by-destination', async (ctx, next) => {
+router.post('/api/prices/by-destination', async (ctx, next) => {
     const prices = await requester.send({
         type: 'prices-by-destination',
         destination: ctx.request.body.destination,
@@ -29,7 +27,7 @@ router.post('/prices/by-destination', async (ctx, next) => {
     ctx.body = prices
 })
 
-router.post('/prices/minute', async (ctx, next) => {
+router.post('/api/prices/minute', async (ctx, next) => {
     const prices = await requester.send({
         type: 'price-minute',
         origin: ctx.request.body.origin,
@@ -38,8 +36,7 @@ router.post('/prices/minute', async (ctx, next) => {
     ctx.body = prices
 })
 
-router.post('/calculator/fale-mais', async (ctx, next) => {
-    console.log('REQUEST INCOMING', ctx.request)
+router.post('/api/calculator/fale-mais', async (ctx, next) => {
     const getCostByMinute = async (ctx) => {
         const {
             request: { 
